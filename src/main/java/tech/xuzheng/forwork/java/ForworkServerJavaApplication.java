c85af2aa4ee7a771390a424daec0a62308541a8e
Xu Zheng <slowpolitics@outlook.com>
2024-07-26 08:35:03 +0800

    Initial commit

---
+++ src/main/java/tech/xuzheng/forwork/java/ForworkServerJavaApplication.java
@@ -0,0 +1,13 @@
+package tech.xuzheng.forwork.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForworkServerJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForworkServerJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/xuzheng/forwork/java/ForworkServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.xuzheng.forwork.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForworkServerJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
