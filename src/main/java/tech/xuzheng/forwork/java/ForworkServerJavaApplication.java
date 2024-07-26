e3fd7c0cc9a41818c93e8b2d1d575bf7d8770e01
Xu Zheng <slowpolitics@outlook.com>
2024-07-26 08:35:51 +0800

    pom

--- src/main/java/tech/xuzheng/forwork/java/ForworkServerJavaApplication.java
+++ src/main/java/tech/xuzheng/forwork/java/ForworkServerJavaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ForworkServerJavaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ForworkServerJavaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ForworkServerJavaApplication.class, args);
+    }
 
 }
--- src/test/java/tech/xuzheng/forwork/java/ForworkServerJavaApplicationTests.java
+++ src/test/java/tech/xuzheng/forwork/java/ForworkServerJavaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ForworkServerJavaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
